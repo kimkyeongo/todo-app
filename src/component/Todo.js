f037b9064bd000a7e43ed8d79714fc06887fad9a
dolphins3654 <103363611+dolphins3654@users.noreply.github.com>
2022-09-07 00:39:33 +0900

    change

--- src/component/Todo.js
+++ src/component/Todo.js
@@ -2,30 +2,32 @@ import "./Todo.css";
 import {useState} from "react";
 
     const Todo = () => {
-        const [content, setContent] = useState({
-            idx: 0,
-            content:""
-        });
-
+        const [content, setContent] = useState({});
+        const [contCurrent, setContCurrent] = useState(0);
+        const [dataList, setDataList] = useState([]);
         const onChange = (e) => {
-            setContent({
-                ...content,
-                {content : e.target.value, idx : 1}
-            });
+            setContent({id:contCurrent, content:e.target.value});
         };
 
         // let [inputs, setInputs] = useState();
         //
         // let {item,setItem} = useState();
-        let addFunc = () => {
-            // const {value} = e.target;
-
+        const addFunc = () => {
+            console.info(content);
+            setContCurrent(contCurrent + 1);
+            setDataList({
+                ...dataList,
+                content
+            })
         }
 
         return (
             <div className="Todo-body">
                 <input name="todo-input" onChange={onChange}/>
                 <button onClick={addFunc}>추가</button>
+                <ul>
+                    {dataList.map(info => (<li key={info.id}>info.content</li>))}
+                </ul>
             </div>
         );
     }
